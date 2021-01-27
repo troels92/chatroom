4f79cc9df0389ad762bfe9b8cf6bae0ffdf4a23c
Troels Smidt <Razorsmidt@hotmail.com>
2021-01-27 10:38:52 +0100

    Changed index

--- src/test/java/com/ts/spring/ws/api/SpringBootWebsocketApplicationTests.java
+++ src/test/java/com/ts/spring/ws/api/SpringBootWebsocketApplicationTests.java
@@ -5,12 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootWebsocketApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	assertEquals(1,2);
+
 	}
 
 }
